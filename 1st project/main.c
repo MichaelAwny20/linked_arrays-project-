ce6b32265e4a3db7d9d0df96dfef0324a6df5a7c
Michael Awny <michaelawnyibrahim@gmail.com>
2021-07-10 05:27:48 -0700

    first commit

---
+++ 1st project/main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct student //structure of student information
+{
+    char name[30];//name of student
+    int id;
+    int day;
+    int month;
+    int year;
+    int score;// score of the last year
+
+};
